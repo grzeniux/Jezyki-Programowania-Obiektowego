10099a54570a6988f1dcaa2a381c19d96ed6988e
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-02 17:01:37 +0100

    zad3

--- Kolos_2/zad3.cpp
+++ Kolos_2/zad3.cpp
@@ -1,31 +1,37 @@
 /*
 Jaki będzie wynik działania poniższego programu? (2pkt)
+*/
+#include <iostream>
+using namespace std;
 
 class Base
 {
-	public:
-		virtual void show (void) { cout<<"In Base\n"; }
-		void show(void) {cout<<"In Base\n"; }
+public:
+	virtual void show1(void) { cout << "In Base\n"; }
+    void show2(void) { cout << "In Base\n"; }
+};
+class Derived : public Base
+{
+public:
+    void show1(void) { cout << "In Derived\n"; }
+    void show2(void) { cout << "In Derived\n"; }
 };
 
-class Derived: public Base
-{
-	public:
-		void show1(void) { cout<<"In Derived\n"; }
-		void show2(void) {cout<<"In Derived\n"; }
-};
 
-
-int main(void)
+int main()
 {
-	Base* bp = new Derived();
-	bp -> show1();
-	bp -> show2();
-	return 0;
+    Base* bp = new Derived();
+    bp->show1();
+    bp->show2();
+    return 0;
 }
 
+/*
+wskaźnik bp typu Base*, który wskazuje na obiekt klasy Derived.
+Wywołuje metodę show1 dla obiektu wskazywanego przez bp. 
+Ponieważ show1 jest oznaczona jako virtual, zostanie użyta implementacja z klasy pochodnej Derived.
+
+Wywołuje metodę show2 dla obiektu wskazywanego przez bp. 
+Tutaj wywołana zostanie implementacja z klasy bazowej Base, ponieważ show2 nie jest oznaczona jako virtual.
 */
-
-
-
 
