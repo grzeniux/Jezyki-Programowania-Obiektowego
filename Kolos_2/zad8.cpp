59d937ee90e51a7e6c3f94845c8d5302ff9c1642
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-02 23:55:50 +0100

    zad8

--- Kolos_2/zad8.cpp
+++ Kolos_2/zad8.cpp
@@ -1,10 +1,29 @@
 /*
-Napisz program, który pobierze z wiersza poleceń jeden wyraz, a następnie wypisze na ekranie komunikat czy jest on palindromem czy nie. 
+Napisz program, kt�ry pobierze z wiersza polecen� jeden wyraz, a nastepnie wypisze na ekranie komunikat czy jest on palindromem czy nie. 
 
-Dla przypomnienia palindrom to słowo brzmiące tak samo zarówno wtedy, gdy jest czytane od lewej do prawej, 
+Dla przypomnienia palindrom to slowo brzmiace tak samo zar�wno wtedy, gdy jest czytane od lewej do prawej, 
 jak i wtedy gdy jest czytanie na odwrót. 
-Na przykład słowa “owocowo”, “radar”, “sedes” są palindromami, zaś słowa “dom” oraz “komputer” - nie są. 
-Program powinien sprawdzić czy wprowadzone słowo jest palindromem, za pomocą funkcji rekurencyjnej (3 pkt)
+Na przyk�ad s�owa owocowo, radar, sedes s� palindromami, za� s�owa dom oraz komputer - nie s� 
+Program powinien sprawdza� czy wprowadzone s�owo jest palindromem, za pomoc� funkcji rekurencyjnej (3 pkt)
 */
 
+#include <iostream>
 
+using namespace std;
+
+bool palindrome(string text)
+{
+	if (text.size() <= 1)
+		return true;
+	else
+	{
+		if (text[0] == text[text.size() - 1])
+			return palindrome(text.substr(1, text.size() - 2));
+		else
+			return false;
+	}
+}
+
+int main(){
+    cout << palindrome("assa") << endl;
+}
