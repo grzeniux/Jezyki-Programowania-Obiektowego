4d2fe551bb4231c1824ad9fb3f015820d3eb1947
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-02 23:33:19 +0100

    zad7

--- Kolos_2/zad7.cpp
+++ Kolos_2/zad7.cpp
@@ -4,6 +4,45 @@ Podczas operacji na pliku może się zdarzyć, że zostanie zgłoszony wyjątek 
 std::ifstream::failure. umieść w programie kod, który przechwytuje ten wyjątek i wyświetla na ekranie odpowiedni komunikat. (3 pkt)
 */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    try {
+        ifstream plik("zad7_plik.txt");     // Otwieranie pliku
+
+        // Sprawdzanie, czy plik został poprawnie otwarty
+        if (!plik.is_open()) {
+            throw runtime_error("Nie udalo sie otworzyc pliku.");
+        }
+
+        string linia;
+        getline(plik, linia);
+
+        // Odwrotna kolejnosc
+        cout << "Oryginalna linia: " << linia << endl;
+        cout << "Linia w odwrotnej kolejnosci liter: ";
+        for (int i = linia.length() - 1; i >= 0; i--) {
+            cout << linia[i];
+        }
+        cout << endl;
+
+        // Zamykanie pliku
+        plik.close();
+    } 
+    
+    catch (const ifstream::failure& e) {
+        cerr << "Blad podczas operacji na pliku: " << e.what() << endl;
+    } 
+    catch (const runtime_error& e) {
+        cerr << "Blad: " << e.what() << endl;
+    }
+
+    return 0;
+}
 
 
 
