26935cfd8e884816f75d6b3501943d457c00daad
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-02 15:15:55 +0100

    zad1

--- Kolos_2/zad2.cpp
+++ Kolos_2/zad2.cpp
@@ -7,5 +7,29 @@ d) usunie środkowy element wektora (przyjmij, że w wektorze jest nieparzysta l
 e) przyjmie logiczną wartość true w przypadku, gdy wektor jest pusty (5 pkt)
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main(){
+
+vector<int> v = { 9, 9, 9, 9, 9 };
+
+v[0] = -1;
+v[v.size()-2] = 1;
+v.insert(v.begin() + 2, 5);
+v.erase(v.begin() + v.size() / 2);
+
+bool is_empty = v.empty();
+cout<<"Czy wektor jest pusty?: "<<is_empty<<endl;
+
+for (int element : v) { 
+    cout << element << " "; }
+cout << endl;
+
+return 0;
+
+
+
+}
 
