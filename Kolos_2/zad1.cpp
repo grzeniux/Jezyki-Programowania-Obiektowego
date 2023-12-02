8e9e02c2ecfc20560ee8b7482956eceb7a48d110
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-02 23:55:36 +0100

    zad1

--- Kolos_2/zad1.cpp
+++ Kolos_2/zad1.cpp
@@ -1,5 +1,13 @@
 /*
-W pewnym projekcie programista utworzyÅ‚ za pomocÄ… dziedziczenia klasÄ™ Motor z klasy Rower. 
+W pewnym projekcie programista utworzyl‚ za pomocÄ… dziedziczenia klasÄ™ Motor z klasy Rower. 
 Czy jest to poprawnie zastosowanie funkcjonalnoÅ›ci dziedziczenia klas? OdpowiedÅº uzasadnij. (1 pkt)
+
+Zastosowanie dziedziczenia klas, gdzie klasa Motor dziedziczy po klasie Rower, nie jest zazwyczaj w³aœciwym podejœciem. 
+Dziedziczenie klas powinno byæ u¿ywane, gdy istnieje logiczna hierarchia miêdzy klasami, 
+gdzie podklasa jest szczególnym przypadkiem nadrzêdnej klasy i dziedziczy jej w³aœciwoœci i metody, 
+rozszerzaj¹c je lub nadpisuj¹c w odpowiedni sposób. Dziedziczenie to bardziej u¿yteczne w sytuacjach, 
+gdzie wystêpuje relacja typu "jest rodzajem"
+
 */
+
 
