c889354c8d2bd5291091524d169d6196ccd32aaf
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-12-03 23:16:58 +0100

    a

--- Kolos_2/zad2.cpp
+++ Kolos_2/zad2.cpp
@@ -6,15 +6,11 @@ c) wstawi do wektora nową wartość 5 na trzecim miejscu, licząc od początku,
 d) usunie środkowy element wektora (przyjmij, że w wektorze jest nieparzysta liczba elementów),
 e) przyjmie logiczną wartość true w przypadku, gdy wektor jest pusty (5 pkt)
 */
-
 #include <iostream>
 #include <vector>
 using namespace std;
-
 int main(){
-
 vector<int> v = { 9, 9, 9, 9, 9 };
-
 v[0] = -1;
 v[v.size()-2] = 1;
 v.insert(v.begin() + 2, 5);
@@ -26,10 +22,6 @@ cout<<"Czy wektor jest pusty?: "<<is_empty<<endl;
 for (int element : v) { 
     cout << element << " "; }
 cout << endl;
-
 return 0;
-
-
-
 }
 
